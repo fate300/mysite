2163d9c70231d8d051a8ab3e1dd57b6eaa69fce2
강지행 <jey78v@naver.com>
2023-07-16 11:24:24 +0900

    mysite 7월16일

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,31 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div class="container">
+    <div class="item color1">item1 Lorem, ipsum dolor sit amet consectetur adipisicing elit. Nisi necessitatibus fugiat qui labore, reprehenderit autem sequi aliquam ad beatae repellendus!</div>
+    <div class="item color2">item2</div>
+    <div class="item color3">item3</div>
+    <div class="item color4">item4</div>
+    <div class="item color5">item5</div>
+    <div class="item color1">item6</div>
+    <div class="item color2">item7</div>
+    <div class="item color3">item8</div>
+    <div class="item color4">item9</div>
+    <div class="item color5">item10</div>
+
+
+
+
+  </div>
+
+
+
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    
   }
 }
 </script>
@@ -22,5 +38,45 @@ export default {
   text-align: center;
   color: #2c3e50;
   margin-top: 60px;
+}
+*  {
+  box-sizing: border-box;
+}
+
+body {
+  width: 100vw;
+  height: 100vh;
+  margin: 0;
+}
+.container {
+display: grid;
+grid-template-columns: repeat(5, 1fr);
+/* colum을 설정하고 row는 auto 그리고 minmax를 이요하는 게 좋음  */
+grid-auto-rows: minmax(150px,auto) ;
+}
+
+.item {
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  border: 1px solid #181818;
+  font-size: 1.2rem;
+  font-weight: bold;
+}
+
+.color1 {
+  background-color: #d7bee2;
+}
+.color2 {
+  background-color: #a9c7d8;
+}
+.color3 {
+  background-color: #c0df9f;
+}
+.color4 {
+  background-color: #f2e5a6;
+}
+.color5 {
+  background-color: #e89d9d;
 }
 </style>
